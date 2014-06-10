04629c7c2fdb652fd1206c770a63a437605111ab
unknown <tanj3@NCI-01870532-L.nci.nih.gov>
2014-06-10 10:24:31 -0400

    JR1035 check script updated

--- db-sql/check_workflow_status.sql
+++ db-sql/check_workflow_status.sql
@@ -1,13 +1,83 @@
 SELECT 
---DISTINCT a.asl_name --production sql
-DISTINCT a.asl_name, c.actl_name  --just for test
+DISTINCT a.asl_name --production sql
+--DISTINCT a.asl_name, c.actl_name  --just for test
 	FROM   ac_status_lov_view a, asl_actl_view_ext c
 	WHERE  a.asl_name = c.asl_name (+)
-	and    (c.actl_name = nvl('DATAELEMENT', c.actl_name) OR c.actl_name = nvl('DE_CONCEPT', c.actl_name))  --just for test
+	and    c.actl_name in ('DATAELEMENT','DE_CONCEPT')  --just for test
 --and c.asl_name = 'RELEASED' --just for test
 	ORDER BY upper(a.asl_name);
 
   
-select * FROM ac_status_lov_view;
+select 
+--count(*)
+* 
+FROM ac_status_lov_view where asl_name in (
+'APPRVD FOR TRIAL USE'
+,'CMTE APPROVED'
+,'CMTE SUBMTD'
+,'CMTE SUBMTD USED'
+,'DRAFT MOD'
+,'DRAFT NEW'
+,'RELEASED'
+,'RELEASED-NON-CMPLNT'
+,'RETIRED ARCHIVED'
+,'RETIRED DELETED'
+,'RETIRED PHASED OUT'
+,'RETIRED WITHDRAWN'
+);
 
-select * FROM asl_actl_view_ext;
+select 
+--count(*)
+* 
+FROM ac_status_lov where asl_name in (
+'APPRVD FOR TRIAL USE'
+,'CMTE APPROVED'
+,'CMTE SUBMTD'
+,'CMTE SUBMTD USED'
+,'DRAFT MOD'
+,'DRAFT NEW'
+,'RELEASED'
+,'RELEASED-NON-CMPLNT'
+,'RETIRED ARCHIVED'
+,'RETIRED DELETED'
+,'RETIRED PHASED OUT'
+,'RETIRED WITHDRAWN'
+);
+
+select 
+--count(*)
+* 
+FROM asl_actl_view_ext where asl_name in (
+'APPRVD FOR TRIAL USE'
+,'CMTE APPROVED'
+,'CMTE SUBMTD'
+,'CMTE SUBMTD USED'
+,'DRAFT MOD'
+,'DRAFT NEW'
+,'RELEASED'
+,'RELEASED-NON-CMPLNT'
+,'RETIRED ARCHIVED'
+,'RETIRED DELETED'
+,'RETIRED PHASED OUT'
+,'RETIRED WITHDRAWN'
+)
+and    actl_name in ('DATAELEMENT','DE_CONCEPT');
+
+select 
+--count(*)
+* 
+FROM asl_actl_ext where asl_name in (
+'APPRVD FOR TRIAL USE'
+,'CMTE APPROVED'
+,'CMTE SUBMTD'
+,'CMTE SUBMTD USED'
+,'DRAFT MOD'
+,'DRAFT NEW'
+,'RELEASED'
+,'RELEASED-NON-CMPLNT'
+,'RETIRED ARCHIVED'
+,'RETIRED DELETED'
+,'RETIRED PHASED OUT'
+,'RETIRED WITHDRAWN'
+)
+and    actl_name in ('DATAELEMENT','DE_CONCEPT');
