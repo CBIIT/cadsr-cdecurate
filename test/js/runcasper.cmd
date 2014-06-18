13ac2896b4c4a83086f1e365cfcab1d3cbb382f1
unknown <tanj3@NCI-01870532-L.nci.nih.gov>
2014-06-18 12:09:18 -0400

    JR1036

--- test/js/runcasper.cmd
+++
@@ -1,7 +0,0 @@
-del *.png
-:casperjs test functions-cas.js
-:casperjs --verbose --log-level=debug test functions-cas.js
-:casperjs test casperjs\JR1036.js --u= --p=
-casperjs test casperjs\JR1036.js --ignore-ssl-errors=true --u= --p=
-:casperjs test casperjs\JR1036.js --u= --p= --t=qa
-:casperjs test casperjs\JR1036.js --u= --p= --t=local
