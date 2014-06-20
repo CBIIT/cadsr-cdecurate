facc61630289b8e50e3739efe8a8b0ffe0f3a62e
unknown <tanj3@NCI-01870532-L.nci.nih.gov>
2014-06-20 13:27:10 -0400

    Refactored functional test into a common module for reuse

--- test/js/casperjs/JR1036.js
+++ test/js/casperjs/JR1036.js
@@ -3,110 +3,27 @@
 /*==============================================================================*/
 
 "use strict";
-var userid;
-var password;
-var tier;
-var x = require('casper').selectXPath;
-casper.options.viewportSize = {width: 1366, height: 643};
-casper.on('page.error', function(msg, trace) {
-    this.echo('Error: ' + msg, 'ERROR');
-    for(var i=0; i<trace.length; i++) {
-        var step = trace[i];
-        this.echo('   ' + step.file + ' (line ' + step.line + ')', 'ERROR');
-    }
-});
-//===c.f. http://casperjs.readthedocs.org/en/latest/cli.html
-casper.echo("Casper CLI passed args:");
-require("utils").dump(casper.cli.args);
+var host, uri;
+var user = {
+    id: '',
+    password: '',
+    tier: ''
+};
+var caspermod = require('casper');
+var utils = require("utils");
+var cdecurate = require("./common.js");
 
-casper.echo("Casper CLI passed options:");
-require("utils").dump(casper.cli.options);
+cdecurate.init(caspermod, utils, user);
 
 casper.test.begin('JR1036 test', function(test) {
-    // removing default options passed by the Python executable
-    casper.cli.drop("cli");
-    casper.cli.drop("casper-path");
-//    if (casper.cli.args.length === 3 && Object.keys(casper.cli.options).length === 3) {
-//        casper.echo("No arg/option passed").exit();
-//    } else {
-        userid = casper.cli.get("u");
-        password = casper.cli.get("p");
-        tier = casper.cli.get("t");
-//    }
-
-    var host, uri;
-    if(typeof tier !== 'undefined') {
-        if(tier !== "local") {
-            host = "http://" + "cdecurate-" + tier + ".nci.nih.gov";
-            uri = "/cdecurate/NCICurationServlet?reqType=logout";
-        } else {
-            host = "http://localhost:8080";
-            uri = "/cdecurate";
-        }
-    }
-    else
-    if(typeof tier === 'undefined') {
-        tier = '*** production ***';
-        host = 'https://' + "cdecurate.nci.nih.gov";
-        uri = "/cdecurate/NCICurationServlet?reqType=logout'";
-    }
-    casper.echo("TARGETED HOST = [" + host + uri + "] tier [" + tier + "]");
-
-//    casper.start('https://cdecurate.nci.nih.gov/cdecurate/NCICurationServlet?reqType=homePage');
-    /* Begin of login steps */
-    casper.start(host + uri);
-    casper.waitForSelector(x("//a[normalize-space(text())='Login']"),
-        function success() {
-            casper.captureSelector("v.png", "html");
-            test.assertExists(x("//a[normalize-space(text())='Login']"), "Login STEP 1");
-            this.click(x("//a[normalize-space(text())='Login']"));
-        },
-        function fail() {
-            test.assertExists(x("//a[normalize-space(text())='Login']"));
-        });
-    casper.waitForSelector("form[name=LoginForm] input[name='Username']",
-        function success() {
-            test.assertExists("form[name=LoginForm] input[name='Username']", "Login STEP 2");
-            this.click("form[name=LoginForm] input[name='Username']");
-        },
-        function fail() {
-            test.assertExists("form[name=LoginForm] input[name='Username']");
-        });
-    casper.waitForSelector("form[name=LoginForm] input[name='Username']",
-        function success() {
-            test.assertExists("form[name=LoginForm] input[name='Username']", "Login STEP 3");
-            this.click("form[name=LoginForm] input[name='Username']");
-        },
-        function fail() {
-            test.assertExists("form[name=LoginForm] input[name='Username']");
-        });
-    casper.waitForSelector("input[name='Username']",
-        function success() {
-            this.sendKeys("input[name='Username']", userid);
-        },
-        function fail() {
-            test.assertExists("input[name='Username']");
-        });
-    casper.waitForSelector("input[name='Password']",
-        function success() {
-            this.sendKeys("input[name='Password']", password + "\r");
-        },
-        function fail() {
-            test.assertExists("input[name='Password']");
-        });
-    casper.waitForSelector("form[name=LoginForm] input[type=submit][value='Login']",
-        function success() {
-            test.assertExists("form[name=LoginForm] input[type=submit][value='Login']", "Login STEP 4");
-            this.click("form[name=LoginForm] input[type=submit][value='Login']");
-        },
-        function fail() {
-            test.assertExists("form[name=LoginForm] input[type=submit][value='Login']");
-        });
-    /* End of login steps */
+    var x = caspermod.selectXPath;
 
 //    casper.wait(1000);
 //    casper.echo("clicking on the menu ...");
     casper
+        .then(function() {
+            cdecurate.connect(caspermod, user);
+        })
         .then(function() {
             this.capture('login.png', {
                 top: 0,
---
+++ test/js/casperjs/common.js
@@ -0,0 +1,101 @@
+'use strict';
+exports.init = function(caspermod, utils, user) {
+    var x = caspermod.selectXPath;
+    casper.options.viewportSize = {width: 1366, height: 643};
+    casper.on('page.error', function(msg, trace) {
+        this.echo('Error: ' + msg, 'ERROR');
+        for(var i=0; i<trace.length; i++) {
+            var step = trace[i];
+            this.echo('   ' + step.file + ' (line ' + step.line + ')', 'ERROR');
+        }
+    });
+    //===c.f. http://casperjs.readthedocs.org/en/latest/cli.html
+    casper.echo("Casper CLI passed args:");
+    utils.dump(casper.cli.args);
+
+    casper.echo("Casper CLI passed options:");
+    utils.dump(casper.cli.options);
+
+    // removing default options passed by the Python executable
+    casper.cli.drop("cli");
+    casper.cli.drop("casper-path");
+//    if (casper.cli.args.length === 3 && Object.keys(casper.cli.options).length === 3) {
+//        casper.echo("No arg/option passed").exit();
+//    } else {
+    user.id = casper.cli.get("u");
+    user.password = casper.cli.get("p");
+    user.tier = casper.cli.get("t");
+//    }
+
+    var host, uri;
+    if(typeof user.tier !== 'undefined') {
+        if(user.tier !== "local") {
+            host = "http://" + "cdecurate-" + user.tier + ".nci.nih.gov";
+            uri = "/cdecurate/NCICurationServlet?reqType=logout";
+        } else {
+            host = "http://localhost:8080";
+            uri = "/cdecurate";
+        }
+    }
+    else
+    if(typeof user.tier === 'undefined') {
+        user.tier = '*** production ***';
+        host = 'https://' + "cdecurate.nci.nih.gov";
+        uri = "/cdecurate/NCICurationServlet?reqType=logout'";
+    }
+    casper.start(host + uri);
+    casper.echo("TARGETED HOST = [" + host + uri + "] tier [" + user.tier + "]");
+}
+
+exports.connect = function(caspermod, user) {
+    var x = caspermod.selectXPath;
+    /* Begin of login steps */
+    casper.waitForSelector(x("//a[normalize-space(text())='Login']"),
+        function success() {
+            casper.captureSelector("v.png", "html");
+            casper.test.assertExists(x("//a[normalize-space(text())='Login']"), "Login STEP 1");
+            this.click(x("//a[normalize-space(text())='Login']"));
+        },
+        function fail() {
+            casper.test.assertExists(x("//a[normalize-space(text())='Login']"));
+        });
+    casper.waitForSelector("form[name=LoginForm] input[name='Username']",
+        function success() {
+            casper.test.assertExists("form[name=LoginForm] input[name='Username']", "Login STEP 2");
+            this.click("form[name=LoginForm] input[name='Username']");
+        },
+        function fail() {
+            casper.test.assertExists("form[name=LoginForm] input[name='Username']");
+        });
+    casper.waitForSelector("form[name=LoginForm] input[name='Username']",
+        function success() {
+            casper.test.assertExists("form[name=LoginForm] input[name='Username']", "Login STEP 3");
+            this.click("form[name=LoginForm] input[name='Username']");
+        },
+        function fail() {
+            casper.test.assertExists("form[name=LoginForm] input[name='Username']");
+        });
+    casper.waitForSelector("input[name='Username']",
+        function success() {
+            this.sendKeys("input[name='Username']", user.id);
+        },
+        function fail() {
+            casper.test.assertExists("input[name='Username']");
+        });
+    casper.waitForSelector("input[name='Password']",
+        function success() {
+            this.sendKeys("input[name='Password']", user.password + "\r");
+        },
+        function fail() {
+            casper.test.assertExists("input[name='Password']");
+        });
+    casper.waitForSelector("form[name=LoginForm] input[type=submit][value='Login']",
+        function success() {
+            casper.test.assertExists("form[name=LoginForm] input[type=submit][value='Login']", "Login STEP 4");
+            this.click("form[name=LoginForm] input[type=submit][value='Login']");
+        },
+        function fail() {
+            casper.test.assertExists("form[name=LoginForm] input[type=submit][value='Login']");
+        });
+    /* End of login steps */
+}
