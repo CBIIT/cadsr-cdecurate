eee1cc96083a843b84303e5b8a2939ed6a5f627b
unknown <tanj3@NCI-01870532-L.nci.nih.gov>
2014-11-26 16:41:53 -0500

    JR1016 unit test

--- test/gov/nih/nci/cadsr/cdecurate/test/junit/JR1016.java
+++ test/gov/nih/nci/cadsr/cdecurate/test/junit/JR1016.java
@@ -1,5 +1,6 @@
 package gov.nih.nci.cadsr.cdecurate.test.junit;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.sql.Connection;
@@ -9,6 +10,7 @@ import gov.nih.nci.cadsr.cdecurate.test.helpers.DBUtil;
 import gov.nih.nci.cadsr.cdecurate.tool.DEC_Bean;
 import gov.nih.nci.cadsr.cdecurate.ui.AltNamesDefsSession;
 import gov.nih.nci.cadsr.cdecurate.ui.AltNamesDefsSessionHelper;
+import gov.nih.nci.cadsr.cdecurate.util.AdministeredItemUtil;
 import gov.nih.nci.cadsr.common.TestUtil;
 
 import org.junit.After;
@@ -167,6 +169,27 @@ public class JR1016 {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	@Test
+	public void handleUserSelectedAlternateDefinition() throws Exception {
+		Alternates[] _alts = new Alternates[3];
+		String def1 = "A liquid tissue with the primary function of transporting oxygen and carbon dioxide. It supplies the tissues with nutrients, removes waste products, and contains various components of the immune system defending the body against infection.";
+		String def2 = "A liquid tissue with the primary function of transporting oxygen and carbon dioxide. It supplies the tissues with nutrients, removes waste products, and contains various components of the immune system defending the body against infection.";
+		String def3 = "Any haplorhine primate not belonging to the family Tarsiidae, Hylobatidae, Pongidae, or Hominidae; this does not correspond to any taxon. This group is divided into Old World monkeys (Cercopithecidae) and New World monkeys (Callitrichidae and Cebidae). Many types are used as an experimental model for human disease and drug testing.";
+		
+		for(int i = 0; i < _alts.length ; i++) _alts[i] = new Alternates();
+		_alts[0].setName(def1);
+		_alts[1].setName(def2);
+		_alts[2].setName(def3);
+		AltNamesDefsSession altSession = new AltNamesDefsSession(_alts);
+
+		assertTrue(AdministeredItemUtil.isAlternateDefinitionExists(def1, altSession));
+		assertTrue(AdministeredItemUtil.isAlternateDefinitionExists(def3, altSession));
+		assertFalse(AdministeredItemUtil.isAlternateDefinitionExists("Def 112", altSession));
+		assertFalse(AdministeredItemUtil.isAlternateDefinitionExists("Def", altSession));
+		assertTrue(AdministeredItemUtil.isAlternateDefinitionExists(def2, altSession));
+		assertTrue(AdministeredItemUtil.isAlternateDefinitionExists(def3, altSession));	
 	}
 
 }
