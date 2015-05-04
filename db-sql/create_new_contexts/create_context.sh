f9b28635e0a1d50a985448385fc41ec480d2acd7
James Tan <james.tan@nih.gov>
2015-05-04 13:40:23 -0400

    CADSRMETA-528 CADSRMETA-529

--- db-sql/create_new_contexts/create_context.sh
+++ db-sql/create_new_contexts/create_context.sh
@@ -9,6 +9,8 @@ export SQLPLUS_PATH=sqlplus
 export HOST=@"(description=(address_list=(address=(protocol=TCP)(host=137.187.181.4)(port=1551)))(connect_data=(SID=DSRDEV)))"
 
 echo 'Creating context ...'
-echo $SQLPLUS_PATH $user/$pwd$HOST @run_create_context.sql 'SDC Pilot Project' 'SDC Pilot Project' 
-$SQLPLUS_PATH $user/$pwd$HOST @run_create_context.sql 'SDC Pilot Project' 'SDC Pilot Project'
+echo $SQLPLUS_PATH $user/$pwd$HOST @run_create_context.sql 'iCaRe2' 'Buffett Cancer Center' 
+$SQLPLUS_PATH $user/$pwd$HOST @run_create_context.sql 'iCaRe2' 'Buffett Cancer Center'
+echo $SQLPLUS_PATH $user/$pwd$HOST @run_create_context.sql 'OCCAM' 'Office of Cancer Complementary and Alternative Medicine' 
+$SQLPLUS_PATH $user/$pwd$HOST @run_create_context.sql 'OCCAM' 'Office of Cancer Complementary and Alternative Medicine'
 echo 'Done'
