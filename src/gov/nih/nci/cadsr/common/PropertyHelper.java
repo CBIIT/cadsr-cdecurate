f4276be069fe5a252291580113114a43f7bc8f7c
lernermh <martin.lerner@nih.gov>
2016-05-19 13:30:49 -0400

    Fix for when DEFAULT_CONTEXT_NAME can not be gotten from cadsrutil.properties.  Wll default to NCIP

--- src/gov/nih/nci/cadsr/common/PropertyHelper.java
+++ src/gov/nih/nci/cadsr/common/PropertyHelper.java
@@ -10,75 +10,95 @@ import java.util.Map;
 import java.util.Properties;
 
 /**
- * This class can be used to retrieve property values that dictates the settings of the application from the database, 
+ * This class can be used to retrieve property values that dictates the settings of the application from the database,
  * as well as the property file. In case of the property file, it is a tier specific file from template.curationtool.properties.
- *
  */
-public class PropertyHelper {
+public class PropertyHelper
+{
 
-	private static org.apache.log4j.Logger _logger = org.apache.log4j.Logger.getLogger(PropertyHelper.class);
-    private static Properties          _propList;
-    private static String              _user;
-    private static String              _pswd;
-	
-	public static String HELP_LINK;
-	public static String LOGO_LINK;
-	public static String EMAIL_ID;
-	public static String EMAIL_PWD;
-	//GF32679
-	public static String DEFAULT_CONTEXT_NAME;
+    private static org.apache.log4j.Logger _logger = org.apache.log4j.Logger.getLogger( PropertyHelper.class );
+    private static Properties _propList;
+    private static String _user;
+    private static String _pswd;
 
-	public static String getHELP_LINK() {
-		return HELP_LINK;
-	}
-	public static void setHELP_LINK(String hELP_LINK) {
-		HELP_LINK = hELP_LINK;
-	}
-	public static String getLOGO_LINK() {
-		return LOGO_LINK;
-	}
-	public static void setLOGO_LINK(String lOGO_LINK) {
-		LOGO_LINK = lOGO_LINK;
-	}
-	public static String getEMAIL_ID() {
-		return EMAIL_ID;
-	}
-	public static void setEMAIL_ID(String eMAIL_ID) {
-		EMAIL_ID = eMAIL_ID;
-	}
-	public static String getEMAIL_PWD() {
-		return EMAIL_PWD;
-	}
-	public static void setEMAIL_PWD(String eMAIL_PWD) {
-		EMAIL_PWD = eMAIL_PWD;
-	}
-	
-	public static String getDatabaseUserID() {
-		return null;	//Database.getString("userid");
-	}
-	
-	public static String getDatabasePassword() {
-		return null;	//Database.getString("password");
-	}
-	
-	public static String getDatabaseURL() {
-		return null;	//Database.getString("jdbcurl");
-	}
+    public static String HELP_LINK;
+    public static String LOGO_LINK;
+    public static String EMAIL_ID;
+    public static String EMAIL_PWD;
+    //GF32679
+    public static String DEFAULT_CONTEXT_NAME;
 
-	public static String getPCSURL() {
-		return CurationToolProperties.getFactory().getProperty("curationtool.links.pcs.url");
-	}
+    public static String getHELP_LINK()
+    {
+        return HELP_LINK;
+    }
 
-	public static String getPCSName() {
-		return CurationToolProperties.getFactory().getProperty("curationtool.links.pcs.name");
-	}
-	
+    public static void setHELP_LINK( String hELP_LINK )
+    {
+        HELP_LINK = hELP_LINK;
+    }
+
+    public static String getLOGO_LINK()
+    {
+        return LOGO_LINK;
+    }
+
+    public static void setLOGO_LINK( String lOGO_LINK )
+    {
+        LOGO_LINK = lOGO_LINK;
+    }
+
+    public static String getEMAIL_ID()
+    {
+        return EMAIL_ID;
+    }
+
+    public static void setEMAIL_ID( String eMAIL_ID )
+    {
+        EMAIL_ID = eMAIL_ID;
+    }
+
+    public static String getEMAIL_PWD()
+    {
+        return EMAIL_PWD;
+    }
+
+    public static void setEMAIL_PWD( String eMAIL_PWD )
+    {
+        EMAIL_PWD = eMAIL_PWD;
+    }
+
+    public static String getDatabaseUserID()
+    {
+        return null;    //Database.getString("userid");
+    }
+
+    public static String getDatabasePassword()
+    {
+        return null;    //Database.getString("password");
+    }
+
+    public static String getDatabaseURL()
+    {
+        return null;    //Database.getString("jdbcurl");
+    }
+
+    public static String getPCSURL()
+    {
+        return CurationToolProperties.getFactory().getProperty( "curationtool.links.pcs.url" );
+    }
+
+    public static String getPCSName()
+    {
+        return CurationToolProperties.getFactory().getProperty( "curationtool.links.pcs.name" );
+    }
+
     /**
      * Load the properties from the XML file specified.
      *
      * @param propFile_ the properties file.
      */
-	/*
+    /*
     public static void loadProp(String propFile_) throws Exception
     {
         _propList = new Properties();
@@ -108,22 +128,37 @@ public class PropertyHelper {
     }
     */
 
-	/**
-	 * GF32649 Default Context change for curation tool
-	 */
-	public static String getDefaultContextName() {
-		if(DEFAULT_CONTEXT_NAME == null) {
-			try {
-				DEFAULT_CONTEXT_NAME = CaDSRUtil.getDefaultContextNameNoCache();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
+    /**
+     * GF32649 Default Context change for curation tool
+     */
+    public static String getDefaultContextName()
+    {
+        if( DEFAULT_CONTEXT_NAME == null )
+        {
+            try
+            {
+                DEFAULT_CONTEXT_NAME = CaDSRUtil.getDefaultContextNameNoCache();
+                _logger.debug( "****** DEFAULT_CONTEXT_NAME = [" + DEFAULT_CONTEXT_NAME + "] from cadsrutil.properties ******" );
+                System.out.println( "****** DEFAULT_CONTEXT_NAME = [" + DEFAULT_CONTEXT_NAME + "] from cadsrutil.properties ******" );
 
-		System.out.println("****** DEFAULT_CONTEXT_NAME = [" + DEFAULT_CONTEXT_NAME + "] from cadsrutil.properties ******");
+            } catch( IOException e )
+            {
+                // MHL FIXME  - just a quick fix
+                if( e.getMessage().contains( "gov.nih.nci.cadsrutil.properties" ) )
+                {
+                    DEFAULT_CONTEXT_NAME = "NCIP";
+                    _logger.debug( "****** DEFAULT_CONTEXT_NAME = [" + DEFAULT_CONTEXT_NAME + "] from internal default ******" );
+                    System.out.println( "****** DEFAULT_CONTEXT_NAME = [" + DEFAULT_CONTEXT_NAME + "] from internal default ******" );
+                }
+                else
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
 
-		return DEFAULT_CONTEXT_NAME;
-	}
+
+        return DEFAULT_CONTEXT_NAME;
+    }
 
 }
