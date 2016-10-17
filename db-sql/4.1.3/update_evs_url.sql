188c4bccc58c9f97463c7fde90deb5f2a89cf0ea
vikramsan <vikram.san@gmail.com>
2016-10-17 10:19:39 -0400

    Updated scripts for CT 4.1.3 with respect to the new EVS variable

--- db-sql/4.1.3/update_evs_url.sql
+++ db-sql/4.1.3/update_evs_url.sql
@@ -1,9 +1,4 @@
-update sbrext.tool_options_view_ext set Value = 'https://lexevsapi6.nci.nih.gov/lexevsapi64'  where Tool_name = 'LexEVSAPI' and Property = 'ACCESS-URL';
-/
-/*
-Comment the above statement and uncomment the below statement to run this SQL update for Curation Tool 4.1.1
-*/
--- update sbrext.tool_options_view_ext set Value = 'http://lexevsapi61.nci.nih.gov/lexevsapi61'  where Tool_name = 'EVSAPI' and Property = 'URL'
+update sbrext.tool_options_view_ext set Value = 'https://lexevsapi6.nci.nih.gov/lexevsapi64'  where Tool_name = 'LexEVSAPI' and Property = 'ACCESS-URL'
 /
 select tool_name, property, VALUE from sbrext.tool_options_view_ext where Tool_name = 'EVSAPI' and Property = 'URL'
 /
