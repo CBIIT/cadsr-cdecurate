d6e4c733ea01d37afd3939b504bafa1c60984805
asafieva <natalia.asafieva@nih.gov>
2016-10-18 16:31:13 -0400

    CURATNTOOL-1246 HTTP to HTTPS link changes

--- db-sql/4.1.3/update-link-urls.sql
+++ db-sql/4.1.3/update-link-urls.sql
@@ -1,5 +1,16 @@
-update SBREXT.TOOL_OPTIONS_EXT set value = 'https' || substr(value, INSTR(value,':',1,1)),
+update SBREXT.TOOL_OPTIONS_EXT set value = 'https' || substr(value, INSTR(value,':',1,1)), DATE_MODIFIED = sysdate, MODIFIED_BY = user
+WHERE value like 'http:%' and property = 'URL' and TOOL_NAME in ('CADSRAPI', 'CURATION', 'FREESTYLE', 'EVSBrowser')
+/
+update SBREXT.TOOL_OPTIONS_EXT set value = 'https' || substr(value, INSTR(value,':',1,1)), DATE_MODIFIED = sysdate, MODIFIED_BY = user
+WHERE value like 'http:%' and 
+((property = 'NEWTERM.URL' and TOOL_NAME = 'EVS')
+or 
+(property = 'CONCEPT.DETAILS.URL' and TOOL_NAME = 'EVSBrowser'))
+/
+update SBREXT.TOOL_OPTIONS_EXT set value = substr(value, 1, instr(value, '/CDEBrowser/', 1, 1)), 
 DATE_MODIFIED = sysdate, MODIFIED_BY = user
-WHERE property = 'URL' and value like 'http:%' and TOOL_NAME in ('CADSRAPI', 'CURATION', 'FREESTYLE', 'EVSBrowser');
-
-commit;
+WHERE
+property = 'URL' and TOOL_NAME = 'CDEBrowser' and value like '%/CDEBrowser/%';
+/
+commit
+/
