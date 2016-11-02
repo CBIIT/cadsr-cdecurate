6888091f3569de62a0b1c98b64606c6e2cfd98e8
asafieva <natalia.asafieva@nih.gov>
2016-11-02 11:51:32 -0400

    ObjectCartAPI changed to use HTTPS

--- db-sql/4.1.3/update-link-urls.sql
+++ db-sql/4.1.3/update-link-urls.sql
@@ -1,5 +1,6 @@
-update SBREXT.TOOL_OPTIONS_EXT set value = 'https' || substr(value, INSTR(value,':',1,1)), DATE_MODIFIED = sysdate, MODIFIED_BY = user
-WHERE value like 'http:%' and property = 'URL' and TOOL_NAME in ('CADSRAPI', 'CURATION', 'FREESTYLE', 'EVSBrowser')
+update SBREXT.TOOL_OPTIONS_EXT set value = 'https' || substr(value, INSTR(value,':',1,1)), 
+DATE_MODIFIED = sysdate, MODIFIED_BY = user
+WHERE value like 'http:%' and property = 'URL' and TOOL_NAME in ('CADSRAPI', 'CURATION', 'FREESTYLE', 'EVSBrowser', 'ObjectCartAPI')
 /
 update SBREXT.TOOL_OPTIONS_EXT set value = 'https' || substr(value, INSTR(value,':',1,1)), DATE_MODIFIED = sysdate, MODIFIED_BY = user
 WHERE value like 'http:%' and 
