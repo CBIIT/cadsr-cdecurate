74d4f0bf0c14ffffe5c6c3aefb6ec2a79861c6a9
asafieva <natalia.asafieva@nih.gov>
2019-04-09 16:29:14 -0400

    java11 docker changes

--- docker11/run.sh
+++ docker11/run.sh
@@ -18,8 +18,8 @@ function wait_for_server() {
 
 echo "=> build application"
 
-echo ant -file build.xml -Ddebug=false -DBRANCH_OR_TAG=${BRANCH_OR_TAG} -Dtiername=${tiername} -DCADSR.DS.USER=${CADSR_DS_USER} -DCADSR.DS.PSWD=******** -DCADSR.DS.TNS.HOST=${CADSR_DS_TNS_HOST} -DTOOL.BASE.DIR=${TOOL_BASE_DIR} -DTEST=false -DCADSR.DS.TNS.SID=${CADSR_DS_TNS_SID} -DJDEBUG=off -DCADSR.DS.TNS.PORT=${CADSR_DS_TNS_PORT} build-product
-ant -file build.xml -Ddebug=false -DBRANCH_OR_TAG=${BRANCH_OR_TAG} -Dtiername=${tiername} -DCADSR.DS.USER=${CADSR_DS_USER} -DCADSR.DS.PSWD=${CADSR_DS_PSWD} -DCADSR.DS.TNS.HOST=${CADSR_DS_TNS_HOST} -DTOOL.BASE.DIR=${TOOL_BASE_DIR} -DTEST=false -DCADSR.DS.TNS.SID=${CADSR_DS_TNS_SID} -DJDEBUG=off -DCADSR.DS.TNS.PORT=${CADSR_DS_TNS_PORT} build-product
+echo ant -file build.xml -Ddebug=false -DBRANCH_OR_TAG=${BRANCH_OR_TAG} -Dtiername=${tiername} -DCADSR.DS.USER=${CADSR_DS_USER} -DCADSR.DS.PSWD=******** -DCADSR.DS.TNS.HOST=${CADSR_DS_TNS_HOST} -DCADSR.DS.TNS.SID=${CADSR_DS_TNS_SID} -DJDEBUG=ON -DCADSR.DS.TNS.PORT=${CADSR_DS_TNS_PORT} -DLINKS.PCS.HOST.URL=${LINKS_PCS_HOST_URL} -LINKS.PCS.NAME=${LINKS_PCS_NAME} dist
+ant -file build.xml -Ddebug=false -DBRANCH_OR_TAG=${BRANCH_OR_TAG} -Dtiername=${tiername} -DCADSR.DS.USER=${CADSR_DS_USER} -DCADSR.DS.PSWD=${CADSR_DS_PSWD} -DCADSR.DS.TNS.HOST=${CADSR_DS_TNS_HOST} -DCADSR.DS.TNS.ENTRY=${CADSR_DS_TNS_ENTRY} -DJDEBUG=ON -DCADSR.DS.TNS.PORT=${CADSR_DS_TNS_PORT} -DLINKS.PCS.HOST.URL=${LINKS_PCS_HOST_URL} "-DLINKS.PCS.NAME=${LINKS_PCS_NAME}" dist
 
 echo "=> starting wildfly in background"
 /opt/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 &
@@ -28,18 +28,19 @@ echo "=> Waiting for the server to boot"
 wait_for_server
 
 echo "=> deploying modules"
-
 cp dist/cdecurate.war /local/content/cdecurate/bin
 cp dist/ojdbc7-12.1.0.2.0.jar /local/content/cdecurate/bin
 
 /opt/wildfly/bin/jboss-cli.sh --file=dist/cdecurate_modules.cli
-/opt/wildfly/bin/jboss-cli.sh --file=dist/cdecurate_deploy.cli
 
 echo "=> reloading wildfly"
 /opt/wildfly/bin/jboss-cli.sh --connect command=:reload
 
 echo "=> Waiting for the server to reload"
 wait_for_server
+
+echo "=> deploying"
+/opt/wildfly/bin/jboss-cli.sh --file=dist/cdecurate_deploy.cli
 
 echo "=> shutting wildfly down"
 /opt/wildfly/bin/jboss-cli.sh --connect command=:shutdown
